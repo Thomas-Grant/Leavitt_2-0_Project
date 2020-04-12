865c1167cf664724a2cb0d36e6789e4d86fdbe0e
quinnjb23 <quinnjb23@gmail.com>
2020-04-12 15:03:47 -0600

    Added queries in comments to be used

--- src/SqlCon.java
+++ src/SqlCon.java
@@ -23,30 +23,47 @@ public class SqlCon {
 	
 	// ### For Double Rooms ###
 	//Get Count Available Housings
+	//SELECT COUNT(HousingID) FROM Housing WHERE Type = double AND Vacancy = available; <- i dont see a thing for this in the database
 	//Get Count All Housings
+	//SELECT COUNT(HousingID) FROM Housing WHERE Type = double;
 	//Get $ Range Housings
+	//SELECT MIN(RentPrice), MAX(RentPrice) FROM Housing WHERE Type = double;
 	//Get $ Average Housings
+	//SELECT AVG(RentPrice) FROM Housing WHERE Type = double;
 	
 	// ### For Single Rooms ###
 	//Get Count Available Housings
+	//SELECT COUNT(HousingID) FROM Housing WHERE Type = single AND Vacancy = available;
 	//Get Count All Housings
+	//SELECT COUNT(HousingID) FROM Housing WHERE Type = single;
 	//Get $ Range Housings
+	//SELECT MIN(RentPrice), MAX(RentPrice) FROM Housing WHERE Type = single;
 	//Get $ Average Housings
+	//SELECT AVG(RentPrice) FROM Housing WHERE Type = single;
 	
 	// ### For Apartments ###
 	//Get Count Available Housings
+	//SELECT COUNT(HousingID) FROM Housing WHERE Type = apartment AND Vacancy = available;
 	//Get Count All Housings
+	//SELECT COUNT(HousingID) FROM Housing WHERE Type = apartment;
 	//Get $ Range Housings
+	//SELECT MIN(RentPrice), MAX(RentPrice) FROM Housing WHERE Type = apartment;
 	//Get $ Average Housings
+	//SELECT AVG(RentPrice) FROM Housing WHERE Type = apartment;
 	
 	// ### For Houses ###
 	//Get Count Available Housings
+	//SELECT COUNT(HousingID) FROM Housing WHERE Type = house AND Vacancy = available;
 	//Get Count All Housings
+	//SELECT COUNT(HousingID) FROM Housing WHERE Type = house;
 	//Get $ Range Housings
+	//SELECT MIN(RentPrice), MAX(RentPrice) FROM Housing WHERE Type = house;
 	//Get $ Average Housings
+	//SELECT AVG(RentPrice) FROM Housing WHERE Type = house;
 	
 	// ### For All ###
 	//Get Count Available Housings
+	//SELECT COUNT(HousingID) FROM Housing WHERE Vacancy = available;
 	//Get Count All Housings
 	public static int getCountAllHousing() {
 		String query = "SELECT COUNT(*) FROM Housing";
